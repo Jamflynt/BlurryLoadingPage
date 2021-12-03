ec20fd49429fa5d2769a40c2b4f59e7c111c7668
Jamie Flynt <jamflynt@gmail.com>
2021-12-02 22:22:39 -0500

    Added comment,

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bg = document.querySelector('.bg');
 
 let load = 0;
 
+// Used to run and set interval for function
 let int = setInterval(blurring, 30)
 
 function blurring() {
@@ -12,10 +13,7 @@ function blurring() {
     }
     loadText.innerText = `${load}%`;
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
-
     bg.style.filter =`blur(${scale(load, 0, 100, 30, 0)}px)`
-
-
 };
 
 
